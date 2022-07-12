372099665efc65441c29f5bc210050680345ad39
Wicho <luiscalderonmiranda@gmail.com>
2022-07-11 19:14:06 -0500

    css rendering well

--- index.js
+++ index.js
@@ -1,34 +1,24 @@
 const http = require('http');
 const { url } = require('inspector');
+const EventEmitter = require('events');
 const fs = require('fs');
 
 const port = process.env.PORT || 8080;
+const aboutEvent = new EventEmitter();
+//const about = document.getElementById('about');
+//console.log(about);
 
-fs.readFile('./index.html', (err, html) => {
+let path = './index.html';
+
+fs.readFile(path, (err, data) => {
   if (err) {
     throw err;
   }
-  fs.readFile('./style.css', (err, css) => {
-    if (err) {
-      throw err;
-    }
-    const server = http.createServer((req, res) => {
-      res.writeHead(200);
-      res.write(html);
-      res.write(css);
-      res.end();
-    }).listen(port);
-  });
+  const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.write(data);
+    res.end();
+  })
+  server.listen(port);
 });
-
-/*const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.write(index);
-  res.end();
-});*/
-
-
-
-/*server.listen(port, () => {
-    console.log('server running');
-});*/
