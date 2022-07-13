6e8143ca926f30bf0b1ee98bbf66eeba068f1129
Wicho <luiscalderonmiranda@gmail.com>
2022-07-13 17:47:49 -0500

    added 404.html, href to other html files working, styles not working

--- index.js
+++ index.js
@@ -3,11 +3,27 @@ const fs = require('fs');
 
 const port = process.env.PORT || 8080;
 
+const index = fs.readFileSync('index.html');
+const about = fs.readFileSync('about.html');
+const contact = fs.readFileSync('contact-me.html');
+const notFound = fs.readFileSync('404.html');
+const style = fs.readFileSync('style.css');
 
 const server = http.createServer((req, res) => {
-  if(req.url === '/') {
-    const index = fs.readFileSync('index.html');
-    res.end(index);
+  const url = req.url;
+  switch (url) {
+    case '/':
+      res.write(index);
+      break;
+    case '/about':
+      res.write(about);
+      break;
+    case '/contact':
+      res.write(contact);
+      break;
+    default:
+      res.write(notFound);
+      break;
   }
   res.end();
 });
