d14966b63b9fe1ba93258460a56e5c4e4904317e
Wicho <luiscalderonmiranda@gmail.com>
2022-07-12 21:32:30 -0500

    home page been send to the user with no problems"
    "
    "

--- index.js
+++ index.js
@@ -1,24 +1,17 @@
 const http = require('http');
-const { url } = require('inspector');
-const EventEmitter = require('events');
 const fs = require('fs');
 
 const port = process.env.PORT || 8080;
-const aboutEvent = new EventEmitter();
-//const about = document.getElementById('about');
-//console.log(about);
 
-let path = './index.html';
 
-fs.readFile(path, (err, data) => {
-  if (err) {
-    throw err;
+const server = http.createServer((req, res) => {
+  if(req.url === '/') {
+    const index = fs.readFileSync('index.html');
+    res.end(index);
   }
-  const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.write(data);
-    res.end();
-  })
-  server.listen(port);
+  res.end();
 });
+
+server.listen(port, () => {
+  console.log('connected');
+})
