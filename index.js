7c6ee2d1bd8b2a2c903883d43e4e33b639ba182f
Wicho <luiscalderonmiranda@gmail.com>
2022-07-15 15:58:28 -0500

    implemented a new solution with backend, write the file with url + html or just the url itself

--- index.js
+++ index.js
@@ -3,27 +3,23 @@ const fs = require('fs');
 
 const port = process.env.PORT || 8080;
 
-const index = fs.readFileSync('index.html');
-const about = fs.readFileSync('about.html');
-const contact = fs.readFileSync('contact-me.html');
-const notFound = fs.readFileSync('404.html');
-const style = fs.readFileSync('style.css');
-
 const server = http.createServer((req, res) => {
   const url = req.url;
-  switch (url) {
-    case '/':
-      res.write(index);
-      break;
-    case '/about':
-      res.write(about);
-      break;
-    case '/contact':
-      res.write(contact);
-      break;
-    default:
-      res.write(notFound);
-      break;
+  if(url === '/'){
+    res.write(fs.readFileSync('index.html'));
+  }
+  else{
+    try{
+      res.write(fs.readFileSync(url.slice(1)+'.html'));
+    }
+    catch{
+      try{
+        res.write(fs.readFileSync(url.slice(1)));
+      }
+      catch{
+        res.write(fs.readFileSync('404.html'));
+      }
+    }
   }
   res.end();
 });
