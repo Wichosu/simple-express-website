2f648838573ac9ebfc84e1382e84357e33028389
Wicho <luiscalderonmiranda@gmail.com>
2022-08-07 19:18:56 -0500

    added genre.js schema and model

---
+++ models/genre.js
@@ -0,0 +1,20 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var genreSchema = new Schema (
+  {
+    name: { type: String, required: true, maxLenght: 100, minLenght: 3},
+  }
+);
+
+//Virtual for genreSchema's URL
+genreSchema
+.virtual('url')
+.get(function () {
+  return '/catalog/genre/' + this._id;
+})
+
+//Export model
+
+module.exports = mongoose.model('genreInstance', genreSchema);
