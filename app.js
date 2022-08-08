c2ecce2c52efd183e4a99ee34f0576b973ac3ab8
Wicho <luiscalderonmiranda@gmail.com>
2022-08-07 21:01:28 -0500

    added controllers

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var app = express();
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://Wicho:<8mzUAR4cVipn82fa>@cluster0.zfbto6y.mongodb.net/locallibrary?retryWrites=true&w=majority';
+var mongoDB = 'mongodb+srv://Wicho:Wicho@cluster0.kcgclqy.mongodb.net/local_library?retryWrites=true&w=majority';
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
