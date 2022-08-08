eef317d36206a49eb5ab57471add5afbfd303c58
Wicho <luiscalderonmiranda@gmail.com>
2022-08-07 22:01:31 -0500

    added catalog.js and routes

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-let coolRouter = require('./routes/cool');
+var catalogRouter = require('./routes/catalog'); //Import routes for "catalog" area of site
 
 var app = express();
 
@@ -29,7 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/users/cool', coolRouter);
+app.use('/catalog', catalogRouter); // Add catalog routes to middleware chain.
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/cool.js
+++
@@ -1,8 +0,0 @@
-let express = require('express')
-let router = express.Router()
-
-router.get('/', (req, res, next) => {
-  res.send('You\'re so cool')
-});
-
-module.exports = router;
