ec575c889410f4cc3c36ec87635a8ba876f9210b
Wicho <luiscalderonmiranda@gmail.com>
2022-08-09 13:46:39 -0500

    added author_list.pug and change author.js

--- models/author.js
+++ models/author.js
@@ -1,3 +1,5 @@
+const { DateTime } = require('luxon');
+
 var mongoose = require('mongoose');
 
 var Schema = mongoose.Schema;
@@ -45,6 +47,25 @@ AuthorSchema
 .virtual('url')
 .get(function () {
   return '/catalog/author/' + this._id;
+});
+
+//Virtual for formatted author's date of birth
+AuthorSchema
+.virtual('date_of_birth_formatted')
+.get(function () {
+  return this.date_of_birth? 
+  DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED)
+  :
+  '';
+});
+
+AuthorSchema
+.virtual('date_of_death_formatted')
+.get(function () {
+  return this.date_of_death? 
+  DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED)
+  :
+  '';
 });
 
 //Export model
