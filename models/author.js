3f537ab3c98d9050495511522453be35c8f35a79
Wicho <luiscalderonmiranda@gmail.com>
2022-08-10 12:07:54 -0500

    modified author.js, author*.pug, lifespan virtual

--- models/author.js
+++ models/author.js
@@ -33,11 +33,15 @@ AuthorSchema
 AuthorSchema.virtual('lifespan').get(function() {
   var lifetime_string = '';
   if (this.date_of_birth) {
-    lifetime_string = this.date_of_birth.getYear().toString();
+    lifetime_string = DateTime
+      .fromJSDate(this.date_of_birth)
+      .toLocaleString(DateTime.DATE_MED);
   }
   lifetime_string += ' - ';
   if (this.date_of_death) {
-    lifetime_string += this.date_of_death.getYear();
+    lifetime_string += DateTime
+      .fromJSDate(this.date_of_death)
+      .toLocaleString(DateTime.DATE_MED);
   }
   return lifetime_string;
 });
@@ -47,25 +51,6 @@ AuthorSchema
 .virtual('url')
 .get(function () {
   return '/catalog/author/' + this._id;
-});
-
-//Virtual for formatted author's date of birth
-AuthorSchema
-.virtual('date_of_birth_formatted')
-.get(function () {
-  return this.date_of_birth? 
-  DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED)
-  :
-  '';
-});
-
-AuthorSchema
-.virtual('date_of_death_formatted')
-.get(function () {
-  return this.date_of_death? 
-  DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED)
-  :
-  '';
 });
 
 //Export model
