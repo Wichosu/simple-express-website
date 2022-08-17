cd098eb65567ed7c4b99fe112974f24dfac34cfa
Wicho <luiscalderonmiranda@gmail.com>
2022-08-17 12:22:06 -0500

    author_update_get on authorController.js

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -186,8 +186,31 @@ exports.author_delete_post = function(req, res) {
 };
 
 //Display Author update form on GET
-exports.author_update_get = function(req, res) {
-  res.send('NOT IMPLEMENTED: Author update GET');
+exports.author_update_get = function(req, res, next) {
+  //Get author for form.
+  async.parallel(
+    {
+      author(callback) {
+        Author.findById(req.params.id).exec(callback);
+      },
+    },
+    (err, results) => {
+      if (err) {
+        return next(err);
+      }
+      if (results.author == null) {
+        //No results
+        const error = new Error('Author not found');
+        error.status = 404;
+        return next(error);
+      }
+      //Success. so render
+      res.render('author_update', {
+        title: 'Update Author',
+        author: results.author,
+      });
+    }
+  );
 };
 
 //Handle Author update on POST
