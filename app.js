315fba5559c6a24bf7c46d785c8ab3c809e5411b
Wicho <luiscalderonmiranda@gmail.com>
2022-08-18 12:35:06 -0500

    added .env

--- app.js
+++ app.js
@@ -17,7 +17,8 @@ app.use(helmet());
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://Wicho:Wicho@cluster0.kcgclqy.mongodb.net/local_library?retryWrites=true&w=majority';
+const dev_db_url = 'mongodb+srv://Wicho:Wicho@cluster0.kcgclqy.mongodb.net/local_library?retryWrites=true&w=majority';
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
