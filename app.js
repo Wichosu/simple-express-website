1c7fb93d4eaa3665b8ecd424c87b2ea3cb1e050c
Wicho <luiscalderonmiranda@gmail.com>
2022-08-18 12:03:39 -0500

    added compression module

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var catalogRouter = require('./routes/catalog'); //Import routes for "catalog" area of site
-
+const compression = require('compression');
+//Create the express application object
 var app = express();
 
 //Set up mongoose connection
@@ -25,6 +26,11 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Compress all routes
+app.use(compression());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
